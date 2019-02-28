1f8d37ab60caa9cf1281e5687fbf313585dd5fdb
MarkLozier <lozierm2@gmail.com>
2019-02-28 16:26:46 -0500

    thursday

---
+++ Eleven.Models/NoteListItem.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Eleven.Models
+{
+   public class NoteListItem
+    {
+        public int NoteId { get; set; }
+        public string Title { get; set; }
+
+        [Display(Name ="Create")]
+        public DateTimeOffset CreatedUtc { get; set; }
+
+        public override string ToString() => Title;
+        
+            
+        
+    }
+}
